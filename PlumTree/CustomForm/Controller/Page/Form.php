dc3cb22167c1c668fd15fcbb1f5695dede8f3feb
root <root@mysite.com>
2018-01-17 16:45:49 +0500

    for validation 1st verson

--- PlumTree/CustomForm/Controller/Page/Form.php
+++ PlumTree/CustomForm/Controller/Page/Form.php
@@ -4,40 +4,71 @@ use \Magento\Framework\App\Action\Context;
 use \Magento\Framework\View\Result\PageFactory;
 use \PlumTree\CustomForm\Model\PostingsFactory;
 use \Magento\Framework\Controller\ResultFactory;
+use \Magento\Framework\Data\Form\FormKey\Validator;
+
 class Form extends \Magento\Framework\App\Action\Action
 {
-    //@ Class variables
+    
+    //@ Injected Class private variables 
 
     protected $resultPageFactory;
     protected $PostingsFactory;
-    protected $title;
-    protected $description;
+    protected $formKeyValidator;
 
-    public function __construct(Context $context,PageFactory $resultPageFactory,PostingsFactory $PostingsFactory)
+    //@ Post variables
+
+    private $data = array('title','description');
+
+    //@ Constructor function 
+    
+    public function __construct(Context $context,PageFactory $resultPageFactory,PostingsFactory $PostingsFactory,Validator  $formKeyValidator)
     {
         $this->resultPageFactory = $resultPageFactory;
-        $this->PostingsFactory= $PostingsFactory;        
+        $this->PostingsFactory= $PostingsFactory;
+        $this->formKeyValidator = $formKeyValidator;        
         parent::__construct($context);
     }
         
     public function execute()
     {
+        
         $post= (array) $this->getRequest()->getPost();
+
+
         if (!empty($post))
         {
-            $this->title= $post['title'];
-            $this->description=$post['description'];
-            $PostingsModel=$this->PostingsFactory->create();
-            $PostingsModel->setData('title',$this->title)->save();
-            $PostingsModel->setData('description',$this->description)->save();           
-            $this->messageManager->addSuccessMessage("Thanks for your feedback.Must naha dho ke!");
-            $resultRedirect=$this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
-            $resultRedirect->setUrl('/customform/page/form');
-            return $resultRedirect;
+            if($this->formKeyValidator->validate($this->_request))
+            {       
+
+                $PostingsModel=$this->PostingsFactory->create();
+                
+                foreach ($this->data as $key )                 
+                {
+                    $PostingsModel->setData($key,$post[$key])->save();
+                }
+
+                $this->messageManager->addSuccessMessage("Thanks for your feedback");
+                $resultRedirect=$this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+                $resultRedirect->setUrl($this->_redirect->getRefererUrl());
+                return $resultRedirect;
+
+            }
+
+            else                
+            {
+                $this->messageManager->addErrorMessage("Invalid form key")  ;
+            }
+
         }
 
+        else
+        {
+            $resultPage = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+            $resultPage->getConfig()->getTitle()->set(__('Custom Form'));
+            return $resultPage;
+        }
         
-        $this->_view->loadLayout();
-        $this->_view->renderLayout();   
     }
+    
+
 }
